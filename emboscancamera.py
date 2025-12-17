65a246347b2bd8492171339e0c3d24b19611d864
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2025-12-17 20:29:51 +0700

    Update emboscancamera.py

--- emboscancamera.py
+++ emboscancamera.py
@@ -1,4 +1,4 @@
-# pages/2_Scan_QR.py
+
 import streamlit as st
 from pyzbar.pyzbar import decode
 from PIL import Image
@@ -7,7 +7,7 @@ st.title("Page 2 — Upload QR Code to Decode")
 
 uploaded = st.file_uploader("Upload a QR code image", type=["png", "jpg", "jpeg"])
 
-# initialize state
+
 if "decoded_text1" not in st.session_state:
     st.session_state.decoded_text1 = ""
 if "decoded_text2" not in st.session_state:
@@ -20,7 +20,7 @@ if uploaded:
     if decoded:
         data = decoded[0].data.decode("utf-8")
 
-        # Expected format: text1|||text2
+        
         if "|||" in data:
             t1, t2 = data.split("|||", 1)
             st.session_state.decoded_text1 = t1
@@ -31,7 +31,7 @@ if uploaded:
     else:
         st.error("No QR code detected.")
 
-# Autofilled fields
+
 text1 = st.text_input("Decoded Field 1", value=st.session_state.decoded_text1)
 text2 = st.text_input("Decoded Field 2", value=st.session_state.decoded_text2)
 
@@ -39,3 +39,4 @@ if st.button("Submit"):
     st.success("Received:")
     st.write("Field 1:", text1)
     st.write("Field 2:", text2)
+
